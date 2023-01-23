f267578ff09c867ebd27a98ed6ab36bfeb5377fa
KanishkaReddy0801 <krishnakanishkareddyalla@gmail.com>
2023-01-23 20:50:17 +0530

    first commit

---
+++ controllers/InstaCloneController.js
@@ -0,0 +1,49 @@
+const InstaCloneService = require("../services/InstaCloneService");
+const fs = require("fs");
+const InstaCloneModel = require("../models/InstaCloneModel");
+
+exports.getPosts = async (req, res) => {
+  try {
+    const blogs = await InstaCloneService.getPosts();
+    res.json({ data: blogs, status: "success" });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+exports.createPost = async (req, res) => {
+  // var img = fs.readFileSync(req.file.path);
+  //   // var encode_img = img.toString('base64');
+  //   var final_img = {
+  //       contentType:req.file.mimetype,
+  //       image:new Buffer.alloc(req.file.size)
+  //   };
+  //   console.log(final_img);
+
+  // try {
+  //   res.contentType(final_img.contentType);
+  //   res.send(final_img.image);
+  //   const blog = await InstaCloneService.createPost(res);
+  //   // res.json({ data: blog, status: "success" });
+  // } catch (err) {
+  //   res.status(500).json({ error: err.message });
+  // }
+
+  var img = fs.readFileSync(req.file.path);
+    var encode_img = img.toString('base64');
+    var final_img = {
+        contentType:req.file.mimetype,
+        image:new Buffer.alloc(req.file.size)
+    };
+    InstaCloneModel.create(final_img,function(err,result){
+        if(err){
+            console.log(err);
+        }else{
+            console.log(result.img);
+            console.log("Saved To database");
+            res.contentType(final_img.contentType);
+            res.send(final_img.image);
+        }
+    })
+
+};
